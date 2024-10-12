4cdce003fd84afc03d8f64c15fd9b2e734a58ce8
MikiraSora <mikirasora0409@126.com>
2024-10-13 02:56:33 +0800

    add more params

--- CustomFumenProviderWebServer/Controllers/FumenController.Public.cs
+++ CustomFumenProviderWebServer/Controllers/FumenController.Public.cs
@@ -1,11 +1,12 @@
 ﻿using CustomFumenProviderWebServer.Databases;
 using CustomFumenProviderWebServer.Models.Responses;
+using CustomFumenProviderWebServer.Models.Tables;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace CustomFumenProviderWebServer.Controllers
 {
-
     [ApiController]
     [Route("fumen")]
     public partial class FumenController : ControllerBase
@@ -26,19 +27,75 @@ namespace CustomFumenProviderWebServer.Controllers
         /// </summary>
         /// <param name="pageIdx">页号 (从0开始)</param>
         /// <param name="countPerPage">每一页显示多少个谱面</param>
+        /// <param name="genre">需要过滤显示的分类，null则表示不过滤</param>
+        /// <param name="order">需要排序的字段，null表示不排序</param>
+        /// <param name="minFilterLevel"></param>
+        /// <param name="maxFilterLevel"></param>
         /// <returns></returns>
         [Route("list")]
         [HttpGet]
-        public async Task<FumenQueryResponse> List(int pageIdx, int countPerPage)
+        public async Task<FumenQueryResponse> List(int pageIdx, int countPerPage, string genre = null, string order = null, float? minFilterLevel = null, float? maxFilterLevel = null)
         {
             var offset = pageIdx * countPerPage;
 
             using var db = await fumenDataDBFactory.CreateDbContextAsync();
 
-            var result = await db.FumenSets.Skip(offset).Take(countPerPage).Include(x => x.FumenDifficults).ToListAsync();
+            IQueryable<FumenSet> list = db.FumenSets;
+
+            if (!string.IsNullOrWhiteSpace(genre))
+            {
+                list = list.Where(x => x.Genre == genre);
+            }
+
+            if (!string.IsNullOrWhiteSpace(order))
+            {
+                list = list.OrderBy(x => EF.Property<FumenSet>(x, order));
+            }
+
+            var fumenSets = await list.Skip(offset).Take(countPerPage).Include(x => x.FumenDifficults).ToListAsync();
+
+            if (minFilterLevel is float minLevel)
+            {
+                for (int setIdx = 0; setIdx < fumenSets.Count; setIdx++)
+                {
+                    var set = fumenSets[setIdx];
+                    for (int diffIdx = 0; diffIdx < set.FumenDifficults.Count; diffIdx++)
+                    {
+                        var diff = set.FumenDifficults.ElementAt(diffIdx);
+                        if (diff.Level < minLevel)
+                        {
+                            set.FumenDifficults.Remove(diff);
+                            diffIdx--;
+                        }
+                    }
+
+                    if (set.FumenDifficults.Count == 0)
+                        fumenSets.RemoveAt(setIdx--);
+                }
+            }
+
+            if (maxFilterLevel is float maxLevel)
+            {
+                for (int setIdx = 0; setIdx < fumenSets.Count; setIdx++)
+                {
+                    var set = fumenSets[setIdx];
+                    for (int diffIdx = 0; diffIdx < set.FumenDifficults.Count; diffIdx++)
+                    {
+                        var diff = set.FumenDifficults.ElementAt(diffIdx);
+                        if (diff.Level > maxLevel)
+                        {
+                            set.FumenDifficults.Remove(diff);
+                            diffIdx--;
+                        }
+                    }
+
+                    if (set.FumenDifficults.Count == 0)
+                        fumenSets.RemoveAt(setIdx--);
+                }
+            }
 
             var response = new FumenQueryResponse();
-            response.FumenSets = result;
+            response.FumenSets = fumenSets;
 
             return response;
         }
